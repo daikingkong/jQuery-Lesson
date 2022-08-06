3873b91894e4cebff858d55654a70b9fe0f3b7cf
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-08-06 12:47:05 +0000

    [Finish] MouseEvent

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 $(function(){
-  $('.box1').slideDown(2000,function(){
-    $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '200px',
-    'width': '200px'
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
+
+// $(function(){
+//   $('.box1').slideDown(2000,function(){
+//     $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '200px',
+//     'width': '200px'
+//     }).slideUp();
+//   });
+// });
